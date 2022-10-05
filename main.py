4e5fd5a3ffa8d0925bb2690b9201b944ae05ba83
quyenld9699 <quyenld9699@gmail.com>
2022-10-05 17:48:59 +0700

    update code

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import postes, user
 
-app = FastAPI()
+app = FastAPI(
+    title="TestAppFastAPI",
+    description="App này làm ra với mục đích học FastAPI",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Le Dinh Quyen",
+        "url": "https://github.com/Quyenld9699",
+        "email": "quyenld9699@gmail.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
 
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:3333",
 ]
 app.add_middleware(
     CORSMiddleware,
